ef1d5256cbab157ab4c60d65fe637125b9f30d60
Samuel Chibueze <lightworthng@gmail.com>
2020-10-09 22:05:19 +0100

    Updates

---
+++ precache-manifest.484e82b36e889dc110b98ca0f743229e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c2c666b0501fe2f1e51d661775655c",
+    "url": "/shopover/index.html"
+  },
+  {
+    "revision": "a594e98801eb537fee18",
+    "url": "/shopover/static/js/2.2264909b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopover/static/js/2.2264909b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74864cb7a6a7f342ff57",
+    "url": "/shopover/static/js/main.a1484047.chunk.js"
+  },
+  {
+    "revision": "14ca2ac5a705d80647ea",
+    "url": "/shopover/static/js/runtime-main.bbe176a6.js"
+  }
+]);
